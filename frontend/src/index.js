1341a98d8b6208afc11d1b6cb9c5de1d1ae92eaa
pjdbernardo <52882100+pjdbernardo@users.noreply.github.com>
2026-02-05 16:23:37 +0100

    Create index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <h1>Frontend a funcionar!</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
